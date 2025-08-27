f0de91c539e6f4225e4444296627dcd6d4fa1fb2
Victor <v.esquinasroque@gmail.com>
2025-08-27 15:15:49 +0200

    feat(victor) working verions without counter and type verification check

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,6 +1,7 @@
 import random
 
 continue_game = True
+first_step = True
 program_choice = random.randint(1, 3)
 
 if program_choice == 1:
@@ -15,36 +16,57 @@ else:
 print(
     f"(Debug): program_choice is {program_choice} and its value is {program_choice_value}"
 )
-print(f"Enter a value between 1 and 3: 1 Rock, 2 Paper and 3 Scissors")
-
-try:
-    players_choice = int(input(">"))
-except ValueError:
-    print(f"Non numeric values are not allowed")
 
 while continue_game:
+    
+    #while first_step:
 
-    try:
-        print(f"Enter a value between 1 and 3: 1 Rock, 2 Paper and 3 Scissors")
-        players_choice = int(input(">"))
+        #try:
+    print(f"Enter a value between 1 and 3: 1 Rock, 2 Paper and 3 Scissors")
+    players_choice = int(input(">"))
 
-        if players_choice == 1:
-            players_choice_value = "Rock"
+    if players_choice == 1:
+        players_choice_value = "Rock"
+        first_step = False
 
-        elif players_choice == 2:
-            players_choice_value = "Paper"
+    elif players_choice == 2:
+        players_choice_value = "Paper"
+        first_step = False
 
-        elif players_choice == 3:
-            players_choice_value = "Scissors"
+    elif players_choice == 3:
+        players_choice_value = "Scissors"
+        first_step = False
 
-        else:
-            print(f"Please select a valid value")
-            continue
+    else:
+        print(f"Please select a valid value")
+                
+        # except ValueError:
+        #     print(f"Non numeric values are not allowed")
+        #     continue
+    
+    print(
+        f"The computer has chosen {program_choice_value} and you have chosen {players_choice_value}"
+    )
+    
+    if program_choice == players_choice:
+        print(f"That's a tie!")
+        
+    elif program_choice == 1 and players_choice == 2:
+        print(f"You win!!")
+        
+    elif program_choice == 1 and players_choice == 3:
+        print(f"You lose!")
 
-    except ValueError:
-        print(f"Non numeric values are not allowed")
-        continue
+    elif program_choice == 2 and players_choice == 1:
+        print(f"You lose!")
 
-print(
-    f"(Debug): players_choice is {players_choice} and its value is {players_choice_value}"
-)
+    elif program_choice == 2 and players_choice == 3:
+        print(f"You win!!")
+        
+    elif program_choice == 3 and players_choice == 1:
+        print(f"You win!!")
+
+    elif program_choice == 3 and players_choice == 2:
+        print(f"You lose!!")
+    
+
